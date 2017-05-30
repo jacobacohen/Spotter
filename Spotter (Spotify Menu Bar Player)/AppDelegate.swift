5f40ce7edabe9667f3be9fb31b0e19496384a7a5
Jacob Cohen <jacobcohen@Jacobs-MacBook-Pro.local>
2017-05-29 20:39:30 -0400

    Initial Commit

---
+++ Spotter (Spotify Menu Bar Player)/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Spotter (Spotify Menu Bar Player)
+//
+//  Created by Jacob Cohen on 5/29/17.
+//  Copyright © 2017 Jacob Cohen. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Spotter (Spotify Menu Bar Player)/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Spotter (Spotify Menu Bar Player)
+//
+//  Created by Jacob Cohen on 5/29/17.
+//  Copyright © 2017 Jacob Cohen. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
