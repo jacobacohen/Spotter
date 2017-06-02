a89bc719502d0ba012f103895dc0b4ffa2245a84
Jacob Cohen <jacobcohen@Jacobs-MacBook-Pro.local>
2017-06-01 22:34:55 -0400

    Project Button Skeleton

--- Spotter (Spotify Menu Bar Player)/AppDelegate.swift
+++ Spotter (Spotify Menu Bar Player)/AppDelegate.swift
@@ -11,10 +11,11 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
+    @IBOutlet weak var window: NSWindow!
 
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        //Insert code here
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
--- Spotter (Spotify Menu Bar Player)/ViewController.swift
+++ Spotter (Spotify Menu Bar Player)/ViewController.swift
@@ -5,15 +5,52 @@
 //  Created by Jacob Cohen on 5/29/17.
 //  Copyright © 2017 Jacob Cohen. All rights reserved.
 //
+//Created with great assistance from the following guide by John Mullins:
+//http://www.johnmullins.co/blog/2014/08/08/menubar-app/
+//And the updated version from bjrnt
+//https://gist.github.com/bjrnt/aa6b5d1a86567c5c2c52677ec418b39a
 
 import Cocoa
 
 class ViewController: NSViewController {
 
+    var buttonPresses = 0
+    var statusBar = NSStatusBar.system()
+    var statusBarItem : NSStatusItem = NSStatusItem()
+    var openMenuItem : NSMenuItem = NSMenuItem()
+    var exitMenuItem : NSMenuItem = NSMenuItem()
+    var theMenu = NSMenu()
+    
+    @IBOutlet weak var button: NSButton!
+    @IBOutlet weak var TextTitle: NSTextField!
+    @IBAction func PressButton(_ sender: Any) {
+        buttonPresses += 1
+        setLabelMessage()
+    }
+    
+    override func awakeFromNib() {
+        setLabelMessage()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        statusBarItem = statusBar.statusItem(withLength: -1)
+        statusBarItem.menu = theMenu
+        
+        openMenuItem.title = "Open"
+        openMenuItem.target = self
+        openMenuItem.action = #selector(setWindowVisible)
+        openMenuItem.keyEquivalent = ""
+        
+        exitMenuItem.title = "Quit"
+        exitMenuItem.target = self
+        exitMenuItem.action = #selector(quit)
+        exitMenuItem.keyEquivalent = ""
+        
+        theMenu.addItem(openMenuItem)
+        theMenu.addItem(exitMenuItem)
+        setLabelMessage()
     }
 
     override var representedObject: Any? {
@@ -22,6 +59,17 @@ class ViewController: NSViewController {
         }
     }
 
+    func setLabelMessage(){
+        TextTitle.stringValue = "You've Pressed the button"
+        statusBarItem.button?.title = "Presses: \(buttonPresses)"
+    }
 
+    func setWindowVisible(sender: AnyObject){
+        self.view.window!.orderFrontRegardless()
+    }
+    
+    func quit(sender: AnyObject) {
+        NSApp.terminate(self)
+    }
 }
 
