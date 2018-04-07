f8bd75c75dcc693cae2420d9805a73f56eb9daa3
jacobacohen <jacobacohen@outlook.com>
2018-04-07 00:26:40 -0400

    fresh start for bitcamap

---
+++ old/Spotter (Spotify Menu Bar Player)/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Spotter (Spotify Menu Bar Player)
+//
+//  Created by Jacob Cohen on 5/29/17.
+//  Copyright © 2017 Jacob Cohen. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        //Insert code here
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
